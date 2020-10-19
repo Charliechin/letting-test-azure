bdaf899ec1371435bdf88996fecee048cb0c90a9
Carlos Diaz <charliedn@gmail.com>
2020-10-19 14:21:00 +0100

    Connects to Azure DB

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
 
+const connectDB = require('./config/db');
+
+// Connect DB
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
